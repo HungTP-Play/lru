96702f44c993e9ae672d16cb3e4993b256ad1881
Hung Tran <hung.p.tran@opswat.com>
2023-06-30 16:51:27 +0700

    chore: to create mapper repo

--- mapper/model/model.go
+++ mapper/model/model.go
@@ -2,6 +2,6 @@ package model
 
 type UrlMapping struct {
 	ID       int64  `json:"id" gorm:"primary_key"`
-	ShortUrl string `json:"short_url"`
-	LongUrl  string `json:"long_url"`
+	ShortUrl string `json:"short_url" gorm:"unique,index"`
+	LongUrl  string `json:"long_url"  gorm:"unique,index"`
 }
