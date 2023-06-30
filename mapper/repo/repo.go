812f2d107bdcdba28b9e0fb6689d756393a5379e
Hung Tran <hung.p.tran@opswat.com>
2023-06-30 16:57:52 +0700

    fix: fix repo

--- mapper/repo/repo.go
+++ mapper/repo/repo.go
@@ -1,1 +1,14 @@
 package repo
+
+import "github.com/HungTP-Play/lru/shared"
+
+type UrlMappingRepo struct {
+	db shared.PostgresDB
+}
+
+func NewUrlMappingRepo(connectionString string) *UrlMappingRepo {
+	db := shared.NewPostgresDB(connectionString)
+	return &UrlMappingRepo{
+		db: *db,
+	}
+}
