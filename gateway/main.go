2478a63847bf320a23b32ba6e83536d454686b0b
Hung Tran <hung.p.tran@opswat.com>
2023-06-30 08:30:38 +0700

    chore: fix module

--- gateway/main.go
+++ gateway/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/HungTP-Play/lru/shared"
 )
 
 func main() {
 	fmt.Printf("This is a main %v", "gateway")
 
-	shared.Greeting()
 }
--- shared/http.go
+++ shared/http.go
@@ -1,4 +1,4 @@
-package httpservice
+package shared
 
 import (
 	"context"
